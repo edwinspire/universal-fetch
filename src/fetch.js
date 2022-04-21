9dc70f9e387017f26e3c6bbbcf8222f07f339f22
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2022-04-20 20:55:20 -0500

    Se agregan métodos para simplificar el uso

--- src/fetch.js
+++ src/fetch.js
@@ -24,6 +24,69 @@ module.exports = class uFetch {
     return headers;
   }
 
+  async DELETE(url, init) {
+    init.method = "DELETE";
+    return this.request(url, init);
+  }
+
+  async GET(url, init) {
+    init.method = "GET";
+    return this.request(url, init);
+  }
+
+  async PUT(url, init) {
+    init.method = "PUT";
+    return this.request(url, init);
+  }
+
+  async POST(url, init) {
+    init.method = "POST";
+    return this.request(url, init);
+  }
+
+  async PATCH(url, init) {
+    init.method = "PATCH";
+    return this.request(url, init);
+  }
+
+  async request(url, params) {
+    let data_query = params.query || {};
+    let data_body = params.body || {};
+    let response;
+    if (!params.headers) {
+      params.headers = {
+        "Content-Type": "application/json",
+      };
+    }
+
+    if (!params.method) {
+      params.method = "GET";
+    }
+
+    params.headers = this._addBasicAuthentication(params.headers);
+    try {
+      let searchURL = new URLSearchParams(data_query);
+      let urlq = url + "?" + searchURL.toString();
+
+      response = await fetch(urlq, {
+        method: params.method.toString().toUpperCase(),
+        body: JSON.stringify(data_body),
+        headers: params.headers,
+      });
+      //cache.put(event.request, response.clone());
+      if (this._redirect_in_unauthorized && response.status == 401) {
+        window.location.href = this._redirect_in_unauthorized;
+      }
+
+      return response;
+    } catch (err) {
+      console.log(err);
+      //const response = await cache.match(event.request);
+      if (response) return response;
+      throw err;
+    }
+  }
+
   async put(url, data, headers) {
     let response;
 
